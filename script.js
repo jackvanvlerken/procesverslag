c45488d07aa773f09233411650540b82addd7896
jackvanvlerken <94113138+jackvanvlerken@users.noreply.github.com>
2021-12-12 02:16:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("tester");
 
+// Hamburger menu
+
 var menuButton = document.querySelector("header div button");
 
 menuButton.addEventListener("click", toggleMenu);
@@ -20,5 +22,32 @@ function toggleMenu(event) {
 
     menuButton.classList.toggle("animatie");
     
+}
+
+// Kleur verandering detail element (Winkelmandje pagina)
+
+var deSummary = document.querySelector("ul li details summary");
+
+deSummary.addEventListener("click", openDetail);
+
+function openDetail(event) {
+    
+deSummary.classList.toggle("showDetail");
+
+}
+
+// Dropdown menu (Winkelmandje pagina)
+
+var vragenOverKopen = document.getElementById("vragen_over_kopen");
+
+vragenOverKopen.addEventListener("click", openVragenoverKopen);
+vragenOverKopen.addEventListener('keydown', openVragenoverKopen);
+
+function openVragenoverKopen() {
+  var vragenOverKopenContent = document.querySelector("#vragen_over_kopen ul");
+  vragenOverKopenContent.classList.toggle("active");
+
+  var vragenOverKopenTitel = document.querySelector("#vragen_over_kopen h3");
+  vragenOverKopenTitel.classList.toggle("active");
 }
 

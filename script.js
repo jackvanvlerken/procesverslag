3581cf95912b20d8f5a5138b70c97b3fd5f0bbe3
jackvanvlerken <94113138+jackvanvlerken@users.noreply.github.com>
2022-01-23 18:11:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,50 +4,107 @@ console.log("tester");
 
 var menuButton = document.querySelector("header div button");
 
-menuButton.addEventListener("click", toggleMenu);
+if (menuButton) {
 
-function toggleMenu(event) {
-    
-    var deNav = document.querySelector("nav");
+  menuButton.addEventListener("click", toggleMenu);
 
-    deNav.classList.toggle("toonMenu");
+  function toggleMenu(event) {
+      
+      var deNav = document.querySelector("nav");
 
-    var deHeader = document.querySelector("header div");
+      deNav.classList.toggle("toonMenu");
 
-    deHeader.classList.toggle("zwarteAchtergrond");
+      var deHeader = document.querySelector("header div");
 
-    var winkelwagen = document.querySelector("header div a:nth-child(3)");
+      deHeader.classList.toggle("zwarteAchtergrond");
 
-    winkelwagen.classList.toggle("verdwijn");
+      var winkelwagen = document.querySelector("header div a:nth-child(3)");
 
-    menuButton.classList.toggle("animatie");
-    
+      winkelwagen.classList.toggle("verdwijn");
+
+      menuButton.classList.toggle("animatie");
+      
+  }
+}
+
+// Keydown event home
+
+var homeButton = document.querySelector("header > div a:first-of-type");
+
+homeButton.addEventListener("keydown", logKey);
+
+function logKey(key) {
+  if (key.keyCode == "32"){
+    window.location.replace("index.html");
+  }
+}
+
+// Keydown event winkelwagen
+
+var winkelwagenButton = document.querySelector("header > div a:last-of-type");
+
+winkelwagenButton.addEventListener("keydown", logKey2);
+
+function logKey2(key) {
+  if (key.keyCode == "32"){
+    window.location.replace("index2.html");
+  }
 }
 
 // Kleur verandering detail element (Winkelmandje pagina)
 
 var deSummary = document.querySelector("ul li details summary");
 
-deSummary.addEventListener("click", openDetail);
 
-function openDetail(event) {
+if (deSummary) {
+  deSummary.addEventListener("click", openDetail);
+
+  function openDetail(event) {
     
-deSummary.classList.toggle("showDetail");
+  deSummary.classList.toggle("showDetail");
 
+  }
+
+  function checkKeyPress(key) {
+    if (key.keyCode == "32") {
+      alert("The spacebar has been pressed")
+    }
+  }
 }
 
 // Dropdown menu (Winkelmandje pagina)
 
 var vragenOverKopen = document.getElementById("vragen_over_kopen");
 
-vragenOverKopen.addEventListener("click", openVragenoverKopen);
-vragenOverKopen.addEventListener('keydown', openVragenoverKopen);
+if (vragenOverKopen) {
+  vragenOverKopen.addEventListener("click", openVragenoverKopen);
+  vragenOverKopen.addEventListener("keydown", openVragenoverKopen);
 
-function openVragenoverKopen() {
-  var vragenOverKopenContent = document.querySelector("#vragen_over_kopen ul");
-  vragenOverKopenContent.classList.toggle("active");
 
-  var vragenOverKopenTitel = document.querySelector("#vragen_over_kopen h3");
-  vragenOverKopenTitel.classList.toggle("active");
+  function openVragenoverKopen() {
+    var vragenOverKopenContent = document.querySelector("#vragen_over_kopen ul");
+    vragenOverKopenContent.classList.toggle("active");
+
+    var vragenOverKopenTitel = document.querySelector("#vragen_over_kopen h3");
+    vragenOverKopenTitel.classList.toggle("active");
+  }
 }
 
+// Theme tijd indicator
+
+var huidigetijd = new Date();
+
+var minuten = huidigetijd.getMinutes();
+
+console.log(minuten % 2);
+
+ if (minuten % 2 == 0) {
+   
+   var deAside = document.querySelector("aside");
+
+  if (deAside) {
+   deAside.classList.add("nieuwjaar");
+  }
+ }
+
+ 
